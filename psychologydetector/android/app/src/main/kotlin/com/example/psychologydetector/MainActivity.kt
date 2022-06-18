1e8e1cd39b5bca408421a9cbfbd4fcb45159bb44
RabiaKuran <rabiakuran34@gmail.com>
2022-06-18 19:48:12 +0300

    psychologydetector
    Human face analysis in 7 titles

---
+++ psychologydetector/android/app/src/main/kotlin/com/example/psychologydetector/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.psychologydetector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ psychologydetector/lib/tensorflow.dart
@@ -0,0 +1,178 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+
+class tensorflow extends StatefulWidget {
+  tensorflow({Key? key}) : super(key: key);
+
+  @override
+  State<tensorflow> createState() => _tensorflowState();
+}
+
+class _tensorflowState extends State<tensorflow> {
+  @override
+  List? _outputs;
+  File? _image;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    //initstate widget ilk olusturuldugunda calıstırılır. Bir kez çalışır.
+    super.initState();
+    _loading = true;
+
+    loadModel().then((value) {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
+
+  loadModel() async {
+    //eğittiğim makinemin dosyalarını varlıklar dosyamdan çektim.
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite", //modellerim
+      labels: "assets/labels.txt", //modellerimin başlıkları
+      numThreads: 1,
+    );
+  }
+
+  classifyImage(File image) async {
+    //Model aracılığıyla görüntünün sınıflandırılması için metot oluşturdum
+    var output = await Tflite.runModelOnImage(
+        path: image.path,
+        imageMean: 0.0, //goruntuyu ortalama
+        imageStd: 255.0,
+        numResults: 2,
+        threshold: 0.2,
+        asynch: true);
+    setState(() {
+      _loading = false;
+      _outputs = output!;
+    });
+  }
+
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+
+  pickImage() async {
+    //galerideki resimleri çekmek ve algılamak için çalışan metot
+    var image = await ImagePicker().getImage(source: ImageSource.gallery);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = File(image.path);
+    });
+    classifyImage(
+        _image!); //fotoğrafın durumunu kategorisini anlamak için modelin özelliklerinin bulunduğu metota yolladım.
+  }
+
+  clickImage() async {
+    //kameradan çekilen resimleri almak ve algılamak için çalışan metot
+    var image = await ImagePicker().getImage(source: ImageSource.camera);
+    if (image == null) return null;
+    setState(() {
+      _loading = true;
+      _image = File(image.path);
+    });
+    classifyImage(_image!);
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.yellow[400], //arkaplan rengi
+        elevation: 1.0,
+        title: Text(
+          //başlık kısmım
+          "PSYCHOLOGY DETECTOR",
+          style: TextStyle(
+            fontSize: 17.0,
+            color: Colors.black87,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(children: [
+        //kaydırılabilir arayüz ekledim. Çünkü kameradan resim eklendğinde ekran içinde kapladığı alan çok
+        //büyük olduğu için sığmıyordu.
+        Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              _loading
+                  ? Container(
+                      height: 300,
+                      width: 300,
+                    )
+                  : Container(
+                      margin: EdgeInsets.all(20),
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          _image == null ? Container() : Image.file(_image!),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          _image == null
+                              ? Container()
+                              : _outputs != null
+                                  ? Text(
+                                      _outputs![0][
+                                          "label"], //sonucu modelin hangi sınıfın içerisindeyse onun sonucunu ekrana basıyor.
+                                      style: TextStyle(
+                                          color: Colors.black, fontSize: 20),
+                                    )
+                                  : Container(child: Text(""))
+                        ],
+                      ),
+                    ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.01,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FloatingActionButton(
+                      //galeriden fotoğraf eklemek için aktifleştirdiğim buton.
+                      tooltip: 'PickImage',
+                      onPressed: pickImage,
+                      child: Icon(
+                        Icons.add_a_photo,
+                        size: 20,
+                        color: Colors.white,
+                      ),
+                      backgroundColor: Colors.amber,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FloatingActionButton(
+                      //kullanıcının anlık olarak fotoğraf ekleyebilmesi için aktifleştirdiğim buton.
+                      tooltip: 'ClickImage',
+                      onPressed: clickImage,
+                      child: Icon(
+                        Icons.camera,
+                        size: 20,
+                        color: Colors.white,
+                      ),
+                      backgroundColor: Colors.amber,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ]),
+    );
+  }
+}
